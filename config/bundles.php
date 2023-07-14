9321a2c325ef56d06d7cefef38e07184d307e7c2
tarik.ch <chettouhtarik3@gmail.com>
2023-07-14 11:41:14 +0200

    react and tailwind setup

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\React\ReactBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
